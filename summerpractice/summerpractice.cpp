79363c8195609299dcd27de5b6b04e7b527c0028
peipRej <gleb2211@gmail.com>
2020-06-21 17:47:40 +0300

    Перший комміт

---
+++ summerpractice/summerpractice.cpp
@@ -0,0 +1,31 @@
+﻿#include <stdio.h>
+int main(void) 
+{
+    int sum = 0;
+    int dec[320] = { 1, };
+    int i, j, p, q;
+    for (i = 1; i < 1001; i++) 
+    {
+
+        for (j = 0; j < 302; j++) 
+        {
+            dec[j] = 2 * dec[j];
+        }
+
+        for (q = 0; q < 302; q++)
+        {
+            if (dec[q] > 9) {
+                dec[q] = dec[q] % 10;
+                dec[q + 1] = dec[q + 1] + 1;
+            }
+        }
+
+    }
+
+    for (p = 0; p < 302; p++) 
+    {
+        sum += dec[p];
+    }
+
+    printf("\nsum = %d \n", sum);
+}
